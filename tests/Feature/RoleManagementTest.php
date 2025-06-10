2a7f286a78751f5553bcda4ade642ab017f748fe
mad-sy <alda0003@hz.nl>
2025-06-10 19:54:15 +0200

    role management db fix

--- tests/Feature/RoleManagementTest.php
+++ tests/Feature/RoleManagementTest.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
+use App\Models\Permission;
 use function Pest\Laravel\{actingAs, seed};
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
 
 beforeEach(function () {
     seed();
